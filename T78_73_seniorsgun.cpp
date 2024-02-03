dc246bf4b7f33c788d6d26263dcaa11fb5f1b427
baizhu0414 <15156699185@163.com>
2024-02-03 11:30:50 +0800

    20240203-补0202简单动态规划思想

--- T78_73_seniorsgun.cpp
+++ T78_73_seniorsgun.cpp
@@ -6,13 +6,34 @@ using namespace std;
 long long gun[MAXN];
 long long monster[MAXN];
 
-// true表示不交换顺序 
+// true表示不交换顺序（降序排列）
 bool compareDown(long long x, long long y) {
 	return x>y;
 }
 
 int main() {
-	
+	int T;// 测试样例数量
+	int n, m;// n:枪攻击力数量，m:monster防御力数量
+	cin>> T;
+	for(int t=0; t<T; t++) {
+		cin>> n>> m;
+		for(int i=0; i<n; i++) {
+			cin>> gun[i];
+		}
+		for(int i=0; i<m; i++) {
+			cin>> monster[i];
+		}
+		sort(gun, gun+n, compareDown);
+		sort(monster, monster+m);// 默认升序
+		int answer=0;
+		// 每颗子弹攻击当前最小的怪
+		for(int i=0; i<n; i++) {
+			if(gun[i]>monster[i]) {
+				answer+=(gun[i]-monster[i]);
+			}
+		}
+		cout<< answer<< endl;
+	}
 	return 0;
-} 
+}
 
---
+++ T79_71_dailifuwuqi.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+// XT7.1 代理服务器
+// tip:贪心---每次寻找访问最多ip的proxy地址。
+int process(string proxy[], int n, string ips[], int m) {
+	int count=0;
+	int idx=0;
+	// i++在此处会导致多跳一个ip，在操作i的情况下，考虑while循环，避免此错误。
+	for(int i=0; i<m; ) { 
+		int curMax=0;
+		for(int j=0; j<n; j++) { // 遍历proxy找到当前可以访问最多ip的proxy。
+			int k=i;
+			while(k<m&& proxy[j]!=ips[k]) { // k：ip地址 
+				k++;
+			}
+			if(curMax<k-i) curMax=k-i;
+		}
+		if(curMax==0) { // 哪一次无法往后访问了，则失败。如：只有一个proxy切在ip中发生重复。
+			return -1;
+		}
+		idx+=curMax;
+		i=idx;
+		count++;
+	}
+	return count-1;// 第一次不算切换，因此选中8次，其实切换了7次。 
+}
+
+int main() {
+	string proxy[1001];
+	string ips[5001];
+	int n, m;//n代理服务器个数, m-IP地址
+	while(scanf("%d", &n)!=EOF) {
+		for(int i=0; i<n; i++) {
+			cin>>proxy[i];
+		}
+		scanf("%d", &m);
+		for(int i=0; i<m; i++) {
+			cin>>ips[i];
+		}//数据输入
+		printf("%d", process(proxy, n, ips, m));
+	}
+	return 0;
+}
+
